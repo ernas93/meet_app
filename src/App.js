fb39961717fad6d3d9d5717a4f74f394c7ec1ce1
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-08-04 22:33:51 +0200

    add test and code for NumberOfEvents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <NumberOfEvents></NumberOfEvents>
       <EventList></EventList>
       <CitySearch></CitySearch>
     </div>
--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -10,7 +10,12 @@ describe('<App /> component', () => {
   test('renders list of events', () => {
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
+
   test('render CitySearch', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+  });
+
+  test('render NumberOfEvents', () => {
+    expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
   });
 });
