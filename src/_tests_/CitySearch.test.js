e7ba5b80314b34a0284a8e13474183aea90249e4
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-08-04 21:43:19 +0200

    add test and code for event component

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -28,14 +28,13 @@ describe('<CitySearch /> component', () => {
   });
 
   test('updates list of suggestions correctly when user types in city textbox', async () => {
-    const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
     CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
 
     // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    await user.type(cityTextBox, 'Berlin');
+    await userEvent.type(cityTextBox, 'Berlin');
 
     // filter allLocations to locations matching "Berlin"
     const suggestions = allLocations
@@ -55,19 +54,18 @@ describe('<CitySearch /> component', () => {
   });
 
   test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
-    const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
     CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    await user.type(cityTextBox, 'Berlin');
+    await userEvent.type(cityTextBox, 'Berlin');
 
     // the suggestion's textContent looks like this: "Berlin, Germany"
     const BerlinGermanySuggestion =
       CitySearchComponent.queryAllByRole('listitem')[0];
 
-    await user.click(BerlinGermanySuggestion);
+    await userEvent.click(BerlinGermanySuggestion);
 
     expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
   });
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import Event from '../components/Event';
 import mockData from '../mock-data';
+import userEvent from '@testing-library/user-event';
 
 const mockEvent = mockData[0];
 
@@ -28,5 +29,33 @@ describe('<Event /> Component', () => {
   test('has the button "show details"', () => {
     const button = EventComponent.queryByText('Show Details');
     expect(button).toBeInTheDocument();
+  });
+
+  test('by default event details are hidden', () => {
+    const eventDOM = EventComponent.container.firstChild;
+    const details = eventDOM.querySelector('.details');
+    expect(details).not.toBeInTheDocument();
+  });
+
+  test('show details after user clicks button "show details"', async () => {
+    const user = userEvent.setup();
+    const button = EventComponent.queryByText('Show Details');
+    await user.click(button);
+
+    const eventDOM = EventComponent.container.firstChild;
+    const details = eventDOM.querySelector('.details');
+    expect(details).toBeInTheDocument();
+  });
+
+  test('hide details after user clicks button "hide details"', async () => {
+    const button = EventComponent.queryByText('Show Details');
+    const eventDOM = EventComponent.container.firstChild;
+    await userEvent.click(button);
+
+    const hideButton = EventComponent.queryByText('Hide Details');
+    await userEvent.click(hideButton);
+
+    const details = eventDOM.querySelector('.details');
+    expect(details).not.toBeInTheDocument();
   });
 });
