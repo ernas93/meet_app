9e5fb710f3967d2764e420fb64f0ce0426671e4f
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-08-12 01:46:51 +0200

    add alerts components and when to show alerts

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -7,7 +7,9 @@ import { getEvents, extractLocations } from '../api';
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]} />);
+    CitySearchComponent = render(
+      <CitySearch allLocations={[]} setInfoAlert={() => {}} />
+    );
   });
   test('renders text input', () => {
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -31,7 +33,9 @@ describe('<CitySearch /> component', () => {
   test('updates list of suggestions correctly when user types in city textbox', async () => {
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(
+      <CitySearch allLocations={allLocations} setInfoAlert={() => {}} />
+    );
 
     // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -58,7 +62,11 @@ describe('<CitySearch /> component', () => {
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
     CitySearchComponent.rerender(
-      <CitySearch allLocations={allLocations} setCurrentCity={() => {}} />
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={() => {}}
+        setInfoAlert={() => {}}
+      />
     );
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
